965c9ca30b6c94c79a7ec611d3ec294c9a36b868
REHAN-MD0 <ahmad7661803@gmail.com>
2026-06-23 11:53:24 +0500

    SPIDER-MD

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923207047366',
+    botName: '𝗦𝗣𝗜𝗗𝗘𝗥 𝗠𝗗',
+    ownerName: 'REHAN MD'
+};
